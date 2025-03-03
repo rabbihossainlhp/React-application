01b832928b64d4b8e487a6c1409399fd4abbedbc
Rabbi Hossain <rabbihossainlhp@gmail.com>
2025-03-03 13:20:16 +0600

    update home

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import {User ,  AlignRight , X} from "lucide-react"
+import {User ,  AlignRight , X , ShoppingCart} from "lucide-react"
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -15,38 +15,39 @@ const Navbar = () => {
     }
 
     return (
-        <header className='navbar  bg-blue-100 py-3 flex justify-center items-center shadow-md h-14'>
-            <ul className='w-full md:overflow-hidden md:h-full flex justify-evenly items-center px-4 '>
+        <header className='navbar flex flex-col items-center justify-between  h-fit'>
+            <ul className='bg-blue-100 py-8 h-4 w-full md:overflow-hidden shadow-md flex justify-evenly items-center px-4'>
                 <div className='flex absolute left-1/12'>
-                    <li className=''>
-                        <Link to="/" > 
-                            <img src="/src/assets/Logo.png" 
+                    <li>
+                        <Link to="/">
+                            <img src="/src/assets/images/Logo.png" 
                                 alt="DigiSphere-logo" 
                                 className='w-36 h-auto object-cover background-cover'
                             />
                         </Link>
                     </li>
                 </div>
-                <div className={`absolute  md:-translate-x-0 top-14 md:left-0 bg-blue-200 w-full 
-                        font-bold text-lg md:flex flex-col  md:flex-row justify-evenly md:items-center md:w-2/5 transition-all duration-500      
-                        md:relative md:top-0  md:bg-transparent   ${isNavOpen?"translate-x-0 ":" -translate-x-full"}`}>
+                <div className={`absolute md:-translate-x-0 top-14 md:left-0 bg-blue-200 w-full 
+                        font-bold text-lg md:flex flex-col md:flex-row justify-evenly md:items-center md:w-2/5 transition-all duration-500      
+                        md:relative md:top-0 md:bg-transparent ${isNavOpen ? "translate-x-0" : "-translate-x-full"}`}>
 
-                    <li className='hover:bg-amber-200 h-10 w-full
-                        flex justify-center cursor-pointer items-center transition-duration:2ms
-                    '>
+                    <li className='hover:bg-amber-200 h-10 w-full flex justify-center cursor-pointer items-center transition-duration:2ms'>
                         <Link to="/">Home</Link>
                     </li>
-                    <li className='hover:bg-amber-200 h-10 w-full
-                        flex justify-center cursor-pointer items-center transition-duration:2ms
-                    ' >
+                    <li className='hover:bg-amber-200 h-10 w-full flex justify-center cursor-pointer items-center transition-duration:2ms'>
                         <Link to="/about">About</Link>
                     </li>
-                    <li className='hover:bg-amber-200 h-10 w-full
-                        flex justify-center cursor-pointer items-center
-                        duration-300 ease-in-out
-                    ' >
-                        <Link to="/faqs" >FAQs</Link>
+                    <li className='hover:bg-amber-200 h-10 w-full flex justify-center cursor-pointer items-center duration-300 ease-in-out'>
+                        <Link to="/faqs">FAQs</Link>
                     </li>
+
+                </div>
+                <div className={`${isOpen ? "w-44 h-20 bg-gray-300 rounded-2xl absolute right-1/12 top-14 flex flex-col justify-evenly items-center" : "hidden"}`}>
+                    <li>
+                        <Link to="/signin">Sign In</Link>
+                    </li>
+                    <li></li>
+
                 </div>
                 <div className={`${
                                 isOpen
@@ -76,8 +77,18 @@ const Navbar = () => {
                     </div>
                         
                 </div>
-                
             </ul>
+
+            <nav className='h-14 w-full flex justify-evenly items-center  '>
+                <form className='h-fit text-center w-1/2 sm:block hidden' >
+                    <input type="text"
+                            placeholder='Search product'
+                            className='w-72 rounded-2xl p-1 pl-2.5 focus:outline-none'
+                            style={{border:"1px solid green"}}                            
+                    />
+                </form>
+                <ShoppingCart size={36} className=' cursor-pointer p-1 bg-gray-300 rounded-md' />    
+            </nav>
         </header>
     );  
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
 const Home = () => {
+    const catagory = [
+        'All',
+        'Graphics Card',
+        'Monitor',
+        'Power Supply',
+        'Telivision'
+    ]
     return (
         <div>
-            fdfs
+            <div className="hero "></div>
+            <div className="catagoryContainer ">
+                <div className="flex gap-3">
+                    {
+                        catagory.map((catagory,index)=>{
+                        return (
+                        <button className=' bg-gray-300 px-2 py-1 mt-2 rounded hover:bg-zinc-400 
+                                transition-all  ease-in active:scale-90 cursor-pointer' key={index} > 
+                                {catagory}.
+                        </button>
+                            )
+                        })
+                    }
+                </div>
+                
+            </div>
         </div>
     );
 };
