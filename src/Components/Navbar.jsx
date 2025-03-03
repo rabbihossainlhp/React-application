5cd63caee73ffb6a7bb89eb4c076ed12dc2829bc
Rabbi Hossain <rabbihossainlhp@gmail.com>
2025-03-03 12:12:44 +0600

    edit navigation baar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,14 +4,19 @@ import {User ,  AlignRight} from "lucide-react"
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const[isNavOpen,setIsNavOpen] = useState(false);
 
     const handleUserIcon=()=>{
         setIsOpen(!isOpen);
     };
 
+    const handleNavResponsive = ()=>{
+        setIsNavOpen(!isNavOpen);
+    }
+
     return (
-        <header className='navbar bg-blue-100 py-3 flex justify-center items-center shadow-md h-14'>
-            <ul className='w-full flex justify-evenly items-center px-4 '>
+        <header className='navbar  bg-blue-100 py-3 flex justify-center items-center shadow-md h-14'>
+            <ul className='w-full md:overflow-hidden md:h-full flex justify-evenly items-center px-4 '>
                 <div className='flex absolute left-1/12'>
                     <li className=''>
                         <Link to="/" > 
@@ -22,14 +27,24 @@ const Navbar = () => {
                         </Link>
                     </li>
                 </div>
-                <div className='flex justify-evenly w-3/5 align-center absolute  left-2/12 mx-auto font-bold text-lg '>
-                    <li>
+                <div className={`absolute  md:-translate-x-0 top-14 md:left-0 bg-blue-200 w-full 
+                        font-bold text-lg md:flex flex-col  md:flex-row justify-evenly md:items-center md:w-2/5 transition-all duration-500      
+                        md:relative md:top-0  md:bg-transparent   ${isNavOpen?"translate-x-0 ":" -translate-x-full"}`}>
+
+                    <li className='hover:bg-amber-200 h-10 w-full
+                        flex justify-center cursor-pointer items-center transition-duration:2ms
+                    '>
                         <Link to="/">Home</Link>
                     </li>
-                    <li>
+                    <li className='hover:bg-amber-200 h-10 w-full
+                        flex justify-center cursor-pointer items-center transition-duration:2ms
+                    ' >
                         <Link to="/about">About</Link>
                     </li>
-                    <li>
+                    <li className='hover:bg-amber-200 h-10 w-full
+                        flex justify-center cursor-pointer items-center
+                        duration-300 ease-in-out
+                    ' >
                         <Link to="/faqs" >FAQs</Link>
                     </li>
                 </div>
@@ -51,7 +66,9 @@ const Navbar = () => {
                         className=' bg-gray-500  order-2 text-black rounded-full cursor-pointer w-10 h-10 p-1  '
                         onClick={handleUserIcon}/>
                     <AlignRight size={28} 
-                        className=' bg-gray-500 order-1 text-black rounded-full cursor-pointer w-10 h-10 p-1 md:hidden' />  
+                        className=' bg-gray-500 order-1 text-black rounded-full cursor-pointer w-10 h-10 p-1 md:hidden' 
+                        onClick={handleNavResponsive}/>  
+                        
                 </div>
                 
             </ul>
